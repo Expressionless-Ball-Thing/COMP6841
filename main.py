ad04a254fef237fd371d4091c53023e4b895e16c
Westley <lowestley0409@gmail.com>
2023-10-10 15:38:25 +1100

    results are now outputted to text files instead of stdout, and added a basic cli interface

--- main.py
+++ main.py
@@ -1,95 +1,17 @@
-from json import JSONDecodeError
-from playwright.sync_api import sync_playwright
-from bs4 import BeautifulSoup
 import re
+import click
+from scraper import scraper
 
-site_url = "https://www.frase.io"
+@click.group()
+def cli():
+    pass
 
-def scraper():
-
-    with sync_playwright() as playwright:
-        chromium = playwright.chromium
-        browser = chromium.launch(headless=True)
-        page = browser.new_page()
-        
-        """ 
-        Getting the items
-        """
-        
-        # Subscribe to "request" and "response" events.
-        
-        requests = []
-        responses = []
-        
-        page.on("request", lambda request: requests.append(request) if request not in requests else request)
-        page.on("response", lambda response: responses.append(response) if response not in responses else response)
-        # subscribe to the loading event
-        page.on("load", lambda page: print("page loaded", page.url))
-        page.goto(site_url)
-        
-        # Use Beautiful to parse down all of the html
-        html = BeautifulSoup(page.content(), 'html.parser')
-        
-        # Check that it actually loaded a page
-        if not bool(html.find('html')):
-            print("The page does not exist, it doesn't have a <html> tag")
-            return
-        
-        # get scripts, meta, link
-        scripts = list(html.find_all('script'))
-        meta_tags = list(html.find_all("meta"))
-        links = list(html.find_all("a"))    
-            
-        """ 
-        Listing them out
-        """
-
-        # # Scanning the scripts for their source and id tag
-        print("-------------------------------------SCRIPT TAG----------------------------------------")
-        for script in scripts:
-            if not (script.attrs.get('src') not in [None, ""] and script.attrs.get('id') not in [None, ""]):
-                print(f"script-source: {script.attrs.get('src')}\nid_tag: {script.attrs.get('id')}\n")
-        
-        print("-------------------------------------META----------------------------------------")
-        # Same thing, but now for meta tags
-        for meta in meta_tags:
-            if not (meta.attrs.get('name') not in [None, ""] and meta.attrs.get('property') not in [None, ""]):
-                print(f"meta_name: {meta.attrs.get('name')}\nmeta_property: {meta.attrs.get('property')}\nmeta_content: {meta.attrs.get('content')}\n")
-
-        print("-------------------------------------LINKS---------------------------------------")
-        # Same thing, but not for anchor tags
-        for link in links:
-            if site_url in link.attrs.get('href') or re.compile("^#.*").match(link.attrs.get('href')):
-                print("type: internal link")
-            else:
-                print("type: external link")
-            print(f"href: {link.attrs.get('href')}\nrel: {link.attrs.get('rel')}\n")
-
-        # print("-------------------------------------REQUESTS---------------------------------------")
-        # for request in requests:
-        #     print(f"METHOD: {request.method}")
-        #     print(f"URL: {request.url}")
-        #     print(f"RESOURCE_TYPE: {request.resource_type}")
-        #     print(f"POST_DATA: {request.post_data}")
-        #     print(f"HEADERS: {request.headers}")
-        
-        # for response in responses:
-        #     print(f"URL: {response.url}")
-        #     print(f"HEADERS: {response.headers}")
-        #     print(f"SECURITY: {response.security_details()}")
-        #     try:
-        #         print(f"TEXT: {response.json()}")
-        #     except:
-        #         try:
-        #             print(f"TEXT: {response.text()}")
-        #         except:
-        #             print(f"TEXT: ")
-
-        #     print(f"SERVER: {response.server_addr()}")
-        
-        browser.close()
+@cli.command(help='Analyze the target URL')
+@click.argument('url')
+def analyze(url):
+    scraper(url)
 
 
 if __name__ == "__main__":
-    scraper()
+    cli()
         
---
+++ scraper.py
@@ -0,0 +1,106 @@
+from json import JSONDecodeError
+from playwright.sync_api import sync_playwright
+from bs4 import BeautifulSoup
+import re
+import json
+
+def scraper(url):
+    with sync_playwright() as playwright:
+        chromium = playwright.chromium
+        browser = chromium.launch(headless=True)
+        page = browser.new_page()
+        
+        """ 
+        Getting the items
+        """
+        
+        # Subscribe to "request" and "response" events.
+        
+        requests = []
+        responses = []
+        
+        page.on("request", lambda request: requests.append(request) if request not in requests else request)
+        page.on("response", lambda response: responses.append(response) if response not in responses else response)
+        page.goto(url)
+        
+        # Use Beautiful to parse down all of the html
+        html = BeautifulSoup(page.content(), 'html.parser')
+        
+        # Check that it actually loaded a page
+        if not bool(html.find('html')):
+            print("The page does not exist, it doesn't have a <html> tag")
+            return
+        
+        # get scripts, meta, link
+        scripts = list(html.find_all('script'))
+        meta_tags = list(html.find_all("meta"))
+        links = list(html.find_all("a"))    
+            
+        """ 
+        Listing them out
+        """
+
+        outfile = open(f"analysis_output/output.txt", "w")
+        request_file = open(f"analysis_output/request.txt", "w")
+        response_file = open(f"analysis_output/response.txt", "w")
+
+        # # Scanning the scripts for their source and id tag
+        outfile.write("\n-------------------------------------SCRIPT TAG----------------------------------------\n")
+        for script in filter(lambda script: script.attrs.get('src') not in [None, ""] and script.attrs.get('id') not in [None, ""], scripts):
+            outfile.write(json.dumps({"script-source" : script.attrs.get('src'), "id_tags": script.attrs.get('id')}, indent=4, sort_keys=True))
+            outfile.write("\n")
+        
+        outfile.write("\n-------------------------------------META----------------------------------------\n")
+        # Same thing, but now for meta tags
+        for meta in filter(lambda meta: (meta.attrs.get('name') not in [None, ""] or meta.attrs.get('property') not in [None, ""]), meta_tags):
+            outfile.write(json.dumps({"meta_name" : meta.attrs.get('name'), "meta_property": meta.attrs.get('property'), "meta_content": meta.attrs.get('content')}, indent=4, sort_keys=True))
+            outfile.write("\n")
+
+        outfile.write("\n-------------------------------------LINKS---------------------------------------\n")
+        # Same thing, but not for anchor tags
+        for link in links:
+            link_json = {"href" : link.attrs.get('href'), "rel": link.attrs.get('rel')}
+            if bool(re.compile("^#.*").match(link.attrs.get('href'))):
+                link_json["type"] = "on-site permalink"            
+            elif url in link.attrs.get('href'):
+                link_json["type"] = "internal link"     
+            else:
+                link_json["type"] = "external link"  
+                        
+            outfile.write(json.dumps(link_json, indent=4, sort_keys=True))
+            outfile.write("\n")
+
+        request_file.write("\n-------------------------------------REQUESTS---------------------------------------\n")
+        for request in requests:
+            request_file.write(json.dumps({
+                "METHOD": request.method,
+                "URL": request.url,
+                "HEADERS": request.headers
+            }, indent=4, sort_keys=True))
+            request_file.write("\n")
+        
+        thing = {}
+        
+        
+        response_file.write("\n-------------------------------------RESPONESES---------------------------------------\n")
+        seen = set()
+        for response in [seen.add(d.url) or d for d in responses if d.url not in seen]:
+            response_file.write(json.dumps({
+                "URL" : response.url,
+                "HEADERS": response.headers,
+                "SECURITY": {key: value for key, value in response.security_details().items() if (key in ["issuer", "protocol", "subjectName"])} if response.security_details() is not None else None,
+                "SERVER": response.server_addr()
+            }, indent=4, sort_keys=True))
+            response_file.write("\n")
+            # try:
+            #     print(f"TEXT: {response.json()}")
+            # except:
+            #     try:
+            #         print(f"TEXT: {response.text()}")
+            #     except:
+            #         print(f"TEXT: ")
+
+        browser.close()
+        outfile.close()
+        request_file.close()
+        response_file.close()
